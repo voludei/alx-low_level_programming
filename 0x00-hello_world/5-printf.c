a353f455b8ce72ed2a7df77498d65f3d962927b8
voludei <chevoluntas@gmail.com>
2022-10-13 20:52:40 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Return: Always 0(Success)
+ * */
+int main(void)
+{
+	ptintf("with proper grammar, but the outcome is a piece of art,\n");
+	ruturn (0);
+}

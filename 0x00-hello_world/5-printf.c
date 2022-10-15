96860310e3052eda3b0b734cee606fa6cdeb8207
voludei <chevoluntas@gmail.com>
2022-10-14 18:36:12 -0700

    edited script for 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/**
- * main-Entry point
- * Return: Always 0(Success)
- * */
-int main(void)
+
+int main(void)/* main - Entry Point */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return (0);/* return - Always 0 (success) */
 }

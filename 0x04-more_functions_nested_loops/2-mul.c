65d0f93c6b08e74dc096813b076108a0f634b18f
voludei <chevoluntas@gmail.com>
2022-10-20 19:02:40 -0700

    edited solution to task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * mul - gives the product of a and b
- * @a: variable
- * @b: variable
+ * @a: variable to multiply
+ * @b: variable to be multiplied with
  *
  * Return:a * b
  */
-
 int mul(int a, int b)
 {
 	return (a * b);
 }
+
+
